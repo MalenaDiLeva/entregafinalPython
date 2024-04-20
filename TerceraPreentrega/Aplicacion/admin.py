79b53a2dd7e9f7ee8b1a25acb72775f2fe96f51a
MalenaDiLeva <162948482+MalenaDiLeva@users.noreply.github.com>
2024-04-20 18:23:33 -0300

    Delete TerceraPreentrega directory

--- TerceraPreentrega/Aplicacion/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from.models import *
-admin.site.register(Curso)
-admin.site.register(Alumnos)
-admin.site.register(Profesores)
-admin.site.register(Avatar)
-# Register your models here.
